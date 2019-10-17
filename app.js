45a89584bdb6db78de2bd10898dd9843bc604217
Idand <idanqwe@gmail.com>
2019-10-17 20:16:02 +0300

    carousel and logo changes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 const mongodbpw = process.env.MONGODB_PASSWORD;
-mongoose.connect("mongodb+srv://admin-idan:"+mongodbpw+"@cluster0-gejzc.mongodb.net/TVShowsDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://admin-idan:" + mongodbpw + "@cluster0-gejzc.mongodb.net/TVShowsDB?retryWrites=true&w=majority", {
   useNewUrlParser: true
 });
 
@@ -59,196 +59,152 @@ const TVShow = mongoose.model('TVShow', itemsSchema, 'TVShows_en');
 
 
 app.get("/", function(req, res) {
-  // var mostPopular2 = ["62286", "60735", "456", "1622", "1412",
+  // var mostPopular2 = [
+  //   "62286", "60735", "456", "1622", "1412",
   //   "1434", "62104", "62688", "60625", "60708",
   //   "46298", "46952", "62273", "1416", "1403",
-  //   "69050", "2190", "2734", "62560", "17610"
-  // ];
-  // var mostPopular2 = [
-  //   "2691",
-  //   "77199",
-  //   "1399",
-  //   "73223",
-  //   "48891",
-  //   "4057",
-  //   "63174",
-  //   "93392",
-  //   "71712",
-  //   "46952",
-  //   "4614",
-  //   "79460",
-  //   "76479",
-  //   "90755",
-  //   "60572",
-  //   "44217",
-  //   "75450",
-  //   "1668"
+  //   "69050", "2190", "2734", "62560", "17610",
+  //   "2691","77199","1399","73223","48891",
+  //   "4057","63174","93392","71712","46952",
+  //   "4614","79460","76479","90755","60572",
+  //   "44217","75450","1668"
   // ];
   // mostPopular2.forEach((id) => {
   //   TVShow.findOne({
   //     id: id
   //   }, function(err, foundTVShow) {
   //     if (!err) {
+  //
   //       var name = foundTVShow.name;
   //       var posterPath = (foundTVShow.posterPath == null) ? "/img/coming-soon.png" : "https://image.tmdb.org/t/p/w500" + foundTVShow.posterPath;
+  //       var voteAvg = foundTVShow.voteAvg;
   //       const obj = {
   //         name,
-  //         posterPath
+  //         posterPath,
+  //         voteAvg
   //       };
   //       console.log(obj);
   //     }
   //   });
   // });
 
-  var mostPopular = [{
-      name: 'Fear the Walking Dead',
-      posterPath: 'https://image.tmdb.org/t/p/w500/lZMb3R3e5vqukPbeDMeyYGf2ZNG.jpg'
-    },
-    {
-      name: 'The Flash',
-      posterPath: 'https://image.tmdb.org/t/p/w500/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg'
-    },
-    {
-      name: 'Arrow',
-      posterPath: 'https://image.tmdb.org/t/p/w500/mo0FP1GxOFZT4UDde7RFDz5APXF.jpg'
-    },
-    {
-      name: 'Supernatural',
-      posterPath: 'https://image.tmdb.org/t/p/w500/3iFm6Kz7iYoFaEcj4fLyZHAmTQA.jpg'
-    },
-    {
-      name: 'The Simpsons',
-      posterPath: 'https://image.tmdb.org/t/p/w500/yTZQkSsxUFJZJe67IenRM0AEklc.jpg'
-    },
-    {
-      name: 'The Seven Deadly Sins',
-      posterPath: 'https://image.tmdb.org/t/p/w500/gxTojpKEOtue85EEFlozwRbDXwJ.jpg'
-    },
-    {
-      name: 'Rick and Morty',
-      posterPath: 'https://image.tmdb.org/t/p/w500/qJdfO3ahgAMf2rcmhoqngjBBZW1.jpg'
-    },
-    {
-      name: 'Family Guy',
-      posterPath: 'https://image.tmdb.org/t/p/w500/gBGUL1UTUNmdRQT8gA1LUV4yg39.jpg'
-    },
-    {
-      name: 'Supergirl',
-      posterPath: 'https://image.tmdb.org/t/p/w500/4ka8vAzAFUZFKxWyfGfwVcSXuZo.jpg'
-    },
-    {
-      name: 'Food Wars!: Shokugeki no Soma',
-      posterPath: 'https://image.tmdb.org/t/p/w500/oSK6MoEScP9V4XF0cPvJ0a66urY.jpg'
-    },
-    {
-      name: 'Gotham',
-      posterPath: 'https://image.tmdb.org/t/p/w500/4XddcRDtnNjYmLRMYpbrhFxsbuq.jpg'
-    },
-    {
-      name: 'Hunter x Hunter',
-      posterPath: 'https://image.tmdb.org/t/p/w500/yWBcBIO9OrF3E85C5Arols6QNnG.jpg'
-    },
-    {
-      name: 'Grey\'s Anatomy',
-      posterPath: 'https://image.tmdb.org/t/p/w500/jnsvc7gCKocXnrTXF6p03cICTWb.jpg'
-    },
-    {
-      name: 'Mr. Robot',
-      posterPath: 'https://image.tmdb.org/t/p/w500/oKIBhzZzDX07SoE2bOLhq2EE8rf.jpg'
-    },
-    {
-      name: 'Riverdale',
-      posterPath: 'https://image.tmdb.org/t/p/w500/4X7o1ssOEvp4BFLim1AZmPNcYbU.jpg'
-    },
-    {
-      name: 'The Blacklist',
-      posterPath: 'https://image.tmdb.org/t/p/w500/bgbQCW4fE9b6wSOSC6Fb4FfVzsW.jpg'
-    },
-    {
-      name: 'South Park',
-      posterPath: 'https://image.tmdb.org/t/p/w500/v9zc0cZpy5aPSfAy6Tgb6I1zWgV.jpg'
-    },
-    {
-      name: 'Marvel\'s Agents of S.H.I.E.L.D.',
-      posterPath: 'https://image.tmdb.org/t/p/w500/cXiETfFK1BTLest5fhTLfDLRdL6.jpg'
-    },
-    {
-      name: 'Law & Order: Special Victims Unit',
-      posterPath: 'https://image.tmdb.org/t/p/w500/6t6r1VGQTTQecN4V0sZeqsmdU9g.jpg'
-    },
-    {
-      name: 'NCIS: Los Angeles',
-      posterPath: 'https://image.tmdb.org/t/p/w500/p0doUtM8RhXcAwHs6VmMZMQKpai.jpg'
-    },
-    {
-      name: 'Two and a Half Men',
-      posterPath: 'https://image.tmdb.org/t/p/w500/A9QDK4OWpv41W27kCv0LXe30k9S.jpg'
-    },
-    {
-      name: 'Game of Thrones',
-      posterPath: 'https://image.tmdb.org/t/p/w500/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg'
-    },
-     {
-      name: 'Brooklyn Nine-Nine',
-      posterPath: 'https://image.tmdb.org/t/p/w500/A3SymGlOHefSKbz1bCOz56moupS.jpg'
-    },
-     {
-      name: 'Raising Dion',
-      posterPath: 'https://image.tmdb.org/t/p/w500/jiXQjwxTJvq3x2Z4zbcvCRwYdo2.jpg'
-    },
-     {
-      name: 'Lucifer',
-      posterPath: 'https://image.tmdb.org/t/p/w500/1sBx2Ew4WFsa1YY32vlHt079O03.jpg'
-    },
-     {
-      name: 'Criminal Minds',
-      posterPath: 'https://image.tmdb.org/t/p/w500/7TCwgX7oQKxcWYEhSPRmaHe6ULN.jpg'
-    },
-     {
-      name: 'The Good Doctor',
-      posterPath: 'https://image.tmdb.org/t/p/w500/53P8oHo9cfOsgb1cLxBi4pFY0ja.jpg'
-    },
-     {
-      name: 'Legacies',
-      posterPath: 'https://image.tmdb.org/t/p/w500/rb64COqdpRRfWOc6gWTfC7WxzXP.jpg'
-    },
-     {
-      name: 'The Boys',
-      posterPath: 'https://image.tmdb.org/t/p/w500/dzOxNbbz1liFzHU1IPvdgUR647b.jpg'
-    },
-     {
-      name: 'The I-Land',
-      posterPath: 'https://image.tmdb.org/t/p/w500/Muev370hN3vdjGsC1ip1HcWqbT.jpg'
-    },
-     {
-      name: 'Titans',
-      posterPath: 'https://image.tmdb.org/t/p/w500/eeHI5iBSCOxj4HGSOmFM6azBmwb.jpg'
-    },
-     {
-      name: 'Vikings',
-      posterPath: 'https://image.tmdb.org/t/p/w500/94gP9uXNdbypwCLORjeurlad15Z.jpg'
-    },
-     {
-      name: 'Friends',
-      posterPath: 'https://image.tmdb.org/t/p/w500/7buCWBTpiPrCF5Lt023dSC60rgS.jpg'
-    },
-     {
-      name: 'NCIS',
-      posterPath: 'https://image.tmdb.org/t/p/w500/fi8EvaWtL5CvoielOjjVvTr7ux3.jpg'
-    },
-     {
-      name: 'The Blacklist',
-      posterPath: 'https://image.tmdb.org/t/p/w500/bgbQCW4fE9b6wSOSC6Fb4FfVzsW.jpg'
-    },
-     {
-      name: 'Pokémon',
-      posterPath: 'https://image.tmdb.org/t/p/w500/2pcTUs20ysCdA6DZclaPmGXD6ph.jpg'
-    }
+  var mostPopular = [
+{ name: 'Game of Thrones',
+  posterPath:'https://image.tmdb.org/t/p/w500/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg',
+  voteAvg: 8.1 },
+{ name: 'Fear the Walking Dead',
+  posterPath:'https://image.tmdb.org/t/p/w500/lZMb3R3e5vqukPbeDMeyYGf2ZNG.jpg',
+  voteAvg: 6.3 },
+{ name: 'The Flash',
+  posterPath:'https://image.tmdb.org/t/p/w500/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg',
+  voteAvg: 6.7 },
+{ name: 'The Simpsons',
+  posterPath:'https://image.tmdb.org/t/p/w500/yTZQkSsxUFJZJe67IenRM0AEklc.jpg',
+  voteAvg: 7.1 },
+{ name: 'Arrow',
+  posterPath:'https://image.tmdb.org/t/p/w500/mo0FP1GxOFZT4UDde7RFDz5APXF.jpg',
+  voteAvg: 5.8 },
+{ name: 'Family Guy',
+  posterPath:'https://image.tmdb.org/t/p/w500/gBGUL1UTUNmdRQT8gA1LUV4yg39.jpg',
+  voteAvg: 6.5 },
+{ name: 'The Seven Deadly Sins',
+  posterPath:'https://image.tmdb.org/t/p/w500/gxTojpKEOtue85EEFlozwRbDXwJ.jpg',
+  voteAvg: 7.9 },
+{ name: 'Rick and Morty',
+  posterPath:'https://image.tmdb.org/t/p/w500/qJdfO3ahgAMf2rcmhoqngjBBZW1.jpg',
+  voteAvg: 8.5 },
+{ name: 'Supergirl',
+  posterPath:'https://image.tmdb.org/t/p/w500/4ka8vAzAFUZFKxWyfGfwVcSXuZo.jpg',
+  voteAvg: 5.8 },
+{ name: 'Food Wars!: Shokugeki no Soma',
+  posterPath:'https://image.tmdb.org/t/p/w500/oSK6MoEScP9V4XF0cPvJ0a66urY.jpg',
+  voteAvg: 8.2 },
+{ name: 'Hunter x Hunter',
+  posterPath:'https://image.tmdb.org/t/p/w500/yWBcBIO9OrF3E85C5Arols6QNnG.jpg',
+  voteAvg: 8.4 },
+{ name: 'Gotham',
+  posterPath:'https://image.tmdb.org/t/p/w500/4XddcRDtnNjYmLRMYpbrhFxsbuq.jpg',
+  voteAvg: 6.9 },
+{ name: 'Riverdale',
+  posterPath:'https://image.tmdb.org/t/p/w500/4X7o1ssOEvp4BFLim1AZmPNcYbU.jpg',
+  voteAvg: 7 },
+{ name: 'South Park',
+  posterPath:'https://image.tmdb.org/t/p/w500/v9zc0cZpy5aPSfAy6Tgb6I1zWgV.jpg',
+  voteAvg: 7.8 },
+{ name: 'Lucifer',
+  posterPath:'https://image.tmdb.org/t/p/w500/1sBx2Ew4WFsa1YY32vlHt079O03.jpg',
+  voteAvg: 7.4 },
+{ name: 'Legacies',
+  posterPath:'https://image.tmdb.org/t/p/w500/rb64COqdpRRfWOc6gWTfC7WxzXP.jpg',
+  voteAvg: 7.3 },
+{ name: 'Titans',
+  posterPath:'https://image.tmdb.org/t/p/w500/eeHI5iBSCOxj4HGSOmFM6azBmwb.jpg',
+  voteAvg: 7.5 },
+{ name: 'Two and a Half Men',
+  posterPath:'https://image.tmdb.org/t/p/w500/A9QDK4OWpv41W27kCv0LXe30k9S.jpg',
+  voteAvg: 6.4 },
+{ name: 'The Blacklist',
+  posterPath:'https://image.tmdb.org/t/p/w500/bgbQCW4fE9b6wSOSC6Fb4FfVzsW.jpg',
+  voteAvg: 7 },
+{ name: 'Criminal Minds',
+  posterPath:'https://image.tmdb.org/t/p/w500/7TCwgX7oQKxcWYEhSPRmaHe6ULN.jpg',
+  voteAvg: 7.1 },
+{ name: 'NCIS',
+  posterPath:'https://image.tmdb.org/t/p/w500/fi8EvaWtL5CvoielOjjVvTr7ux3.jpg',
+  voteAvg: 6.7 },
+{ name: 'Vikings',
+  posterPath:'https://image.tmdb.org/t/p/w500/94gP9uXNdbypwCLORjeurlad15Z.jpg',
+  voteAvg: 7.5 },
+{ name: 'Supernatural',
+  posterPath:'https://image.tmdb.org/t/p/w500/3iFm6Kz7iYoFaEcj4fLyZHAmTQA.jpg',
+  voteAvg: 7.3 },
+{ name: 'Law & Order: Special Victims Unit',
+  posterPath:'https://image.tmdb.org/t/p/w500/6t6r1VGQTTQecN4V0sZeqsmdU9g.jpg',
+  voteAvg: 6.5 },
+{ name: 'Marvel\'s Agents of S.H.I.E.L.D.',
+  posterPath:'https://image.tmdb.org/t/p/w500/cXiETfFK1BTLest5fhTLfDLRdL6.jpg',
+  voteAvg: 6.8 },
+{ name: 'Raising Dion',
+  posterPath:'https://image.tmdb.org/t/p/w500/jiXQjwxTJvq3x2Z4zbcvCRwYdo2.jpg',
+  voteAvg: 10 },
+{ name: 'The Boys',
+  posterPath:'https://image.tmdb.org/t/p/w500/dzOxNbbz1liFzHU1IPvdgUR647b.jpg',
+  voteAvg: 8 },
+{ name: 'NCIS: Los Angeles',
+  posterPath:'https://image.tmdb.org/t/p/w500/p0doUtM8RhXcAwHs6VmMZMQKpai.jpg',
+  voteAvg: 6.8 },
+{ name: 'Friends',
+  posterPath:'https://image.tmdb.org/t/p/w500/7buCWBTpiPrCF5Lt023dSC60rgS.jpg',
+  voteAvg: 7.9 },
+{ name: 'Grey\'s Anatomy',
+  posterPath:'https://image.tmdb.org/t/p/w500/jnsvc7gCKocXnrTXF6p03cICTWb.jpg',
+  voteAvg: 6.4 },
+{ name: 'Brooklyn Nine-Nine',
+  posterPath:'https://image.tmdb.org/t/p/w500/A3SymGlOHefSKbz1bCOz56moupS.jpg',
+  voteAvg: 7.6 },
+{ name: 'Mr. Robot',
+  posterPath:'https://image.tmdb.org/t/p/w500/oKIBhzZzDX07SoE2bOLhq2EE8rf.jpg',
+  voteAvg: 7.9 },
+{ name: 'Black Clover',
+  posterPath:'https://image.tmdb.org/t/p/w500/kaMisKeOoTBPxPkbC3OW7Wgt6ON.jpg',
+  voteAvg: 5.6 },
+{ name: 'The Good Doctor',
+  posterPath:'https://image.tmdb.org/t/p/w500/53P8oHo9cfOsgb1cLxBi4pFY0ja.jpg',
+  voteAvg: 7.6 },
+{ name: 'The I-Land',
+  posterPath:'https://image.tmdb.org/t/p/w500/Muev370hN3vdjGsC1ip1HcWqbT.jpg',
+  voteAvg: 5.5 },
+{ name: 'The Blacklist',
+  posterPath:'https://image.tmdb.org/t/p/w500/bgbQCW4fE9b6wSOSC6Fb4FfVzsW.jpg',
+  voteAvg: 7 },
+{ name: 'Pokémon',
+  posterPath:'https://image.tmdb.org/t/p/w500/2pcTUs20ysCdA6DZclaPmGXD6ph.jpg',
+  voteAvg: 6.4 }
   ];
 
-
-  res.render("index", {
-    mostPopular: mostPopular
+  let randomNum = Math.floor(Math.random() * 33) + 2;
+  res.render("index",{
+    mostPopular: mostPopular,randomNum:randomNum
   });
   // TVShow.find({},{name:1},function(err,docs){
   //
@@ -312,13 +268,14 @@ app.get("/TVShows/:TVShowName/:seasonNumber", (req, res) => {
       }
       const season = foundTVShow.seasons[seasonNumber];
       const posterPath = (season.posterPath.length <= 5) ? "/img/coming-soon.png" : "https://image.tmdb.org/t/p/w500" + season.posterPath;
-
+      const numOfSeasons = foundTVShow.seasons.length-1;
       res.render("seasonpage", {
         name: tvShowName,
         posterPath: posterPath,
         seasonName: season.name,
         numOfEpisodes: season.episodes.length,
-        seasonNumber: season.seasonNumber
+        seasonNumber: season.seasonNumber,
+        numOfSeasons:numOfSeasons
       });
 
     }).catch((err) => {
@@ -368,6 +325,7 @@ app.get("/TVShows/:TVShowName/:seasonNumber/:episodeNumber", (req, res) => {
       const overview = (episode.overview.length <= 5) ? "No overview for this episode yet" : episode.overview;
       const episodeImages = episode.episodeImages;
       const guestStars = episode.guestStars;
+      const numOfEpisodes = season.episodeCount;
       const fullEpisodeImages = [];
       var firstTime = true;
       episodeImages.forEach(function(image) {
@@ -390,7 +348,8 @@ app.get("/TVShows/:TVShowName/:seasonNumber/:episodeNumber", (req, res) => {
         airDate: episode.airDate,
         overview: overview,
         episodeImages: fullEpisodeImages,
-        guestStars: guestStars
+        guestStars: guestStars,
+        numOfEpisodes:numOfEpisodes
       });
     }).catch((err) => {
       res.status(400).send(err);
